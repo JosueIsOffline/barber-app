49f3207d6ceb8bfc7d7f0757c403a4a49160a86f
JosueIsOffline <josuehernandez2314@gmail.com>
2025-11-02 17:45:40 -0400

    feat: setup nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex justify-center items-center h-70 w-70 bg-amber-500">
+        <span className="text-5xl font-bold text-black">Orange</span>
+      </div>
+    </div>
+  );
+}
